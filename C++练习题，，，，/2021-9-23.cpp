e993ce3f4eac17c3a911e2fdc6549f792cf0b0b4
WJ-0928 <49092641+WJ-0928@users.noreply.github.com>
2021-09-25 14:13:07 +0800

    Add files via upload

--- C++练习题，，，，/2021-9-23.cpp
+++ C++练习题，，，，/2021-9-23.cpp
@@ -101,42 +101,202 @@ using namespace std;
 //	return 0;
 //}
 
+//
+//template<class T>
+//
+//class SeqList
+//{
+//public:
+//	SeqList(int capacity = 10)
+//	{
+//		capacity = capacity < 10 ? 10 : capacity;
+//		_array = new T[capacity];
+//		_capacity = capacity;
+//		_size = 0;
+//	}
+//	//在类中声明，类外定义
+//	~SeqList() {}
+//private:
+//	T* _array;
+//	int _size;
+//	int _capacity;
+//};
+//template<class T>
+//SeqList<T>::~SeqList()
+//{
+//	if (_array)
+//	{
+//		delete[] _array;
+//		_array = nullptr;
+//		_capacity = 0;
+//		_size = 0;
+//	}
+//}
+//
+//int main()
+//{
+//	// SeqList类名，SeqList<int>才是类型
+//	SeqList<int> s1;
+//
+//	SeqList<double> s2;
+//}
 
-template<class T>
+//
+//void Teststring1()
+//{
+//	// 注意：string类对象支持直接用cin和cout进行输入和输出
+//	string s("hello, bit!!!");
+//	cout << s.size() << endl;
+//	cout << s.length() << endl;
+//	cout << s.capacity() << endl;
+//	cout << s << endl;
+//
+//	// 将s中的字符串清空，注意清空时只是将size清0，不改变底层空间的大小
+//	s.clear();
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//	// 将s中有效字符个数增加到10个，多出位置用'a'进行填充
+//	// “aaaaaaaaaa”
+//	s.resize(10, 'a');
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//	// 将s中有效字符个数增加到15个，多出位置用缺省值'\0'进行填充
+//	// "aaaaaaaaaa\0\0\0\0\0"
+//	// 注意此时s中有效字符个数已经增加到15个
+//	s.resize(15);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//	cout << s << endl;
+//	// 将s中有效字符个数缩小到5个
+//	s.resize(5);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//	cout << s << endl;
+//}
+////================================================================================
+//void Teststring2()
+//{
+//	string s;
+//	// 测试reserve是否会改变string中有效元素个数
+//	s.reserve(100);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//	// 测试reserve参数小于string的底层空间大小时，是否会将空间缩小
+//	s.reserve(50);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//}
+//// 利用reserve提高插入数据的效率，避免增容带来的开销
+////================================================================================
+//void TestPushBack()
+//{
+//	string s;
+//	size_t sz = s.capacity();
+//	cout << "making s grow:\n";
+//	for (int i = 0; i < 100; ++i)
+//	{
+//		s.push_back('c');
+//		if (sz != s.capacity())
+//		{
+//			sz = s.capacity();
+//			cout << "capacity changed: " << sz << '\n';
+//		}
+//	}
+//}
+//void TestPushBackReserve()
+//{
+//	string s;
+//	s.reserve(100);
+//	size_t sz = s.capacity();
+//
+//	cout << "making s grow:\n";
+//	for (int i = 0; i < 100; ++i)
+//	{
+//		s.push_back('c');
+//		if (sz != s.capacity())
+//		{
+//			sz = s.capacity();
+//			cout << "capacity changed: " << sz << '\n';
+//		}
+//	}
+//}
 
-class SeqList
-{
-public:
-	SeqList(int capacity = 10)
-	{
-		capacity = capacity < 10 ? 10 : capacity;
-		_array = new T[capacity];
-		_capacity = capacity;
-		_size = 0;
-	}
-	//在类中声明，类外定义
-	~SeqList() {}
-private:
-	T* _array;
-	int _size;
-	int _capacity;
-};
-template<class T>
-SeqList<T>::~SeqList()
-{
-	if (_array)
-	{
-		delete[] _array;
-		_array = nullptr;
-		_capacity = 0;
-		_size = 0;
-	}
-}
 
-int main()
-{
-	// SeqList类名，SeqList<int>才是类型
-	SeqList<int> s1;
+//void Teststring()
+//{
+//	string s1("hello Bit");
+//	const string s2("Hello Bit");
+//	cout << s1 << " " << s2 << endl;
+//	cout << s1[0] << " " << s2[0] << endl;
+//
+//	s1[0] = 'H';
+//	cout << s1 << endl;
+//
+//	// s2[0] = 'h'; 代码编译失败，因为const类型对象不能修改
+//}
+//void Teststring()
+//{
+//	string s("hello Bit");
+//	// 3种遍历方式：
+//	// 需要注意的以下三种方式除了遍历string对象，还可以遍历是修改string中的字符，
+//	// 另外以下三种方式对于string而言，第一种使用最多
+//	// 1. for+operator[]
+//	for (size_t i = 0; i < s.size(); ++i)
+//		cout << s[i] << endl;
+//
+//	// 2.迭代器
+//	string::iterator it = s.begin();
+//	while (it != s.end())
+//	{
+//		cout << *it << endl;
+//		++it;
+//	}
+//
+//	string::reverse_iterator rit = s.rbegin();
+//	while (rit != s.rend())
+//		cout << *rit << endl;
+//
+//	// 3.范围for
+//	for (auto ch : s)
+//		cout << ch << endl;
+//}
 
-	SeqList<double> s2;
-}
+
+//void Teststring()
+//{
+//	string str;
+//	str.push_back(' '); // 在str后插入空格
+//	str.append("hello"); // 在str后追加一个字符"hello"
+//	str += 'b'; // 在str后追加一个字符'b' 
+//	str += "it"; // 在str后追加一个字符串"it"
+//	cout << str << endl;
+//	cout << str.c_str() << endl; // 以C语言的方式打印字符串
+//
+//	// 获取file的后缀
+//	string file1("string.cpp");
+//	size_t pos = file.rfind('.');
+//	string suffix(file.substr(pos, file.size() - pos));
+//	cout << suffix << endl;
+//
+//	// npos是string里面的一个静态成员变量
+//	// static const size_t npos = -1;
+//
+//	// 取出url中的域名
+//	sring url("http://www.cplusplus.com/reference/string/string/find/");
+//	cout << url << endl;
+//	size_t start = url.find("://");
+//	if (start == string::npos)
+//	{
+//		cout << "invalid url" << endl;
+//		return;
+//	}
+//	start += 3;
+//	size_t finish = url.find('/', start);
+//	string address = url.substr(start, finish - start);
+//	cout << address << endl;
+//
+//	// 删除url的协议前缀
+//	pos = url.find("://");
+//	url.erase(0, pos + 3);
+//	cout << url << endl;
+//}
